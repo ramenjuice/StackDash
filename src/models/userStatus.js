f9d892e435cf6cd999a0b79e45f139c062e3064c
ramenjuice <ramenjuice@users.noreply.github.com>
2015-08-28 18:29:15 -0700

    started adding backend user section controller

---
+++ src/models/userStatus.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {name:"Thomas",id:   , status:false },
+  {name: "Chris",id:   , status:false },
+  {name: "Caleb",id:   , status:false },
+  {name: "Lehi",id:   , status:false} ,
+  {name: "Marshal",id:   , status:false },
+  {name: "Jennifer",id:   , status:false },
+  {name: "Tiffany",id:   , status:false },
+  {name: "Kevin",id:   , status:false} ,
+  {name: "Brian",id:   , status:false },
+  {name: "William",id:   , status:false },
+  {name: "Robert", ,id:   , status:false }
+
+]
--- src/routes/users.js
+++ src/routes/users.js
@@ -7,3 +7,71 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+
+
+
+
+
+// ///TODO add better error handling
+//
+// var express = require('express');
+// var router = express.Router();
+// var userModel = require();
+// /* GET users listing. */
+// router.get('/', function findAll(req, res, next) {
+//   userModel.findAll(function onFound(err, users) {
+//     if (err){
+//       console.log(err);
+//       res.send(500);
+//     }else{
+//       res.render('users',{users:users})
+//     }
+//   });
+// });
+//
+// router.post('/create', function addUser(req,res,next) {
+//   userModel.addUser(req.body.content, function onAdded(err,user) {
+//     if(!err){
+//       ////TODO:test this url
+//       return res.redirect('/');
+//     }
+//     res.send(500);
+//   });
+// });
+//
+// router.get('/edit/:id', function editUser(req, res, next) {
+//   userModel.findAll(function onFound(err, users) {
+//     if(!err){
+//       return res.render('edit',{users:users, current: req.params.id});
+//     }
+//     res.send(500);
+//   });
+// });
+//
+// app.post('/udpate/:id', function updateUser(req,res,next) {
+//   userModel.updateUser(req.params.id, req.body.content, function onUpdated(err) {
+//     if(!err){
+//       return res.redirect('/');
+//     }
+//   });
+// });
+//
+//
+// ///TODO add a delete route
+// /*
+// load data
+// get all
+// get one
+// add one
+// update one
+// delete one
+//
+//
+// */
+//
+//
+//
+//
+// module.exports = router;
